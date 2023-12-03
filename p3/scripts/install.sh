4432a1dac3691e02f2adb2e4a1a0efefdda75883
lrandria <adrian.varnier@proton.me>
2023-12-03 15:25:58 +0100

    on wil-ingress

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 # Update and Upgrade
-# sudo apt-get -y update
-# sudo apt-get -y upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 # Install Docker
-# sudo apt-get install ca-certificates curl gnupg
-# sudo install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get -y update
-# sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-# sudo groupadd docker
-# sudo usermod -aG docker $USER
-# newgrp docker
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 # Install kubectl
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-# sudo mv ./kubectl /usr/local/bin/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 # Install k3d
-# curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 # Install ArgoCD
-# curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
-# sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
-# rm argocd-linux-amd64
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
--- p3/scripts/launch.sh
+++ p3/scripts/launch.sh
@@ -1,10 +1,17 @@
-# k3d cluster create lrandriaC
+k3d cluster create lrandriaC
 
-# kubectl create namespace argocd
-# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create namespace argocd
+kubectl create namespace dev
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-
-# connect to ArgoCD
+# # connect to ArgoCD
 # kubectl port-forward -n argocd svc/argocd-server 8080:443
 # kubectl -n argocd get secret argocd-initial-admin-secret -o=jsonpath="{.data.password}" | base64 --decode; echo
 # argocd login 127.0.0.1:8080
+
+# argocd app create wil-playground --repo https://github.com/adrianvarnier/lrandria-iot.git --path wil-argocd playground --dest-server https://kubernetes.default.svc --dest-namespace dev
+# argocd app sync wil-playground
+# argocd app set wil-playground --sync-policy automated
+# argocd app set wil-playground --auto-prune
+# argocd app set wil-playground --self-heal
+# kubectl apply -f ../confs/ingress.yaml
